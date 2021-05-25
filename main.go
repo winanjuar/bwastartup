b9ed49c155deea37aa03b360dabc893bbfb7e426
Sugeng Winanjuar <winanjuar@gmail.com>
2021-05-25 13:16:52 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bwastartup/handler"
+	"bwastartup/user"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	dsn := "webapps:winanjuar016ka@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	router := gin.Default()
+	api := router.Group("/api/v1")
+	api.POST("/users", userHandler.RegisterUser)
+	router.Run()
+}
+
+// func handler(context *gin.Context) {
+// 	dsn := "webapps:winanjuar016ka@tcp(127.0.0.1:3306)/bwastartup?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatalln(err.Error())
+// 	}
+
+// 	var users []user.User
+// 	db.Find(&users)
+
+// 	context.JSON(http.StatusOK, users)
+// }
+
+// input
+// handler -> mapping input form user ke struct Input
+// service -> mapping struct Input ke struct User
+// repository -> save struct User ke DB
+// db
---
+++ user/input.go
@@ -0,0 +1,13 @@
+package user
+
+type RegisterUserInput struct {
+	Name       string `json:"name" binding:"required"`
+	Occupation string `json:"occupation" binding:"required"`
+	Email      string `json:"email" binding:"required,email"`
+	Password   string `json:"password" binding:"required"`
+}
+
+type LoginUserInput struct {
+	Email    string
+	Password string
+}
