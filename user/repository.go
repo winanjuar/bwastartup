2eb2c117a25228f95b51188249320d3745bdec3a
Sugeng Winanjuar <winanjuar@gmail.com>
2021-05-29 08:48:49 +0700

    Upload avatar end point

--- user/repository.go
+++ user/repository.go
@@ -5,6 +5,8 @@ import "gorm.io/gorm"
 type Repository interface {
 	Save(user User) (User, error)
 	FindByEmail(email string) (User, error)
+	FindByID(ID int) (User, error)
+	Update(user User) (User, error)
 }
 
 type repository struct {
@@ -24,5 +26,15 @@ func (r *repository) FindByEmail(email string) (User, error) {
 	var user User
 	err := r.db.Where("email = ?", email).Find(&user).Error
 	return user, err
+}
 
+func (r *repository) FindByID(ID int) (User, error) {
+	var user User
+	err := r.db.Where("id = ?", ID).Find(&user).Error
+	return user, err
+}
+
+func (r *repository) Update(user User) (User, error) {
+	err := r.db.Save(&user).Error
+	return user, err
 }
