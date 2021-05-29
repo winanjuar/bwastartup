1d7b351882f65f1ae804fee210a1cebad322836e
Sugeng Winanjuar <winanjuar@gmail.com>
2021-05-29 13:10:59 +0700

    Implements JWT

---
+++ auth/service.go
@@ -0,0 +1,25 @@
+package auth
+
+import "github.com/dgrijalva/jwt-go"
+
+type Service interface {
+	GenerateToken(userID int) (string, error)
+}
+
+type jwtService struct {
+}
+
+func NewService() *jwtService {
+	return &jwtService{}
+}
+
+var SECRET_KEY = []byte("BWASTARTUP_s3cr3T_k3Y")
+
+func (s *jwtService) GenerateToken(userID int) (string, error) {
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	signedToken, err := token.SignedString(SECRET_KEY)
+	return signedToken, err
+}
--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwastartup/auth"
 	"bwastartup/helper"
 	"bwastartup/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -36,9 +38,14 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 
-	// token, err := h.jwtService.GenerateToken()
+	token, err := h.authService.GenerateToken(newUser.ID)
+	if err != nil {
+		response := helper.APIResponse("Register account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
 
-	formatter := user.FormatUser(newUser, "tokentokentoken")
+	formatter := user.FormatUser(newUser, token)
 	response := helper.APIResponse("Account has been register", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
@@ -64,7 +71,14 @@ func (h *userHandler) Login(c *gin.Context) {
 		return
 	}
 
-	formatter := user.FormatUser(loggedUser, "tokentokentoken")
+	token, err := h.authService.GenerateToken(loggedUser.ID)
+	if err != nil {
+		response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(loggedUser, token)
 	response := helper.APIResponse("Successfully Logged In", http.StatusOK, "success", formatter)
 	c.JSON(http.StatusOK, response)
 }
