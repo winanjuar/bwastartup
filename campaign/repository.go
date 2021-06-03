774ff2868fa13ca65a46b2cf2c4a4d4ccbf761bb
Sugeng Winanjuar <winanjuar@gmail.com>
2021-06-03 10:37:52 +0700

    campaign repository

--- campaign/repository.go
+++ campaign/repository.go
@@ -1,1 +1,28 @@
 package campaign
+
+import "gorm.io/gorm"
+
+type Repsitory interface {
+	FindAll() ([]Campaign, error)
+	FindByUserID(userID int) ([]Campaign, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindAll() ([]Campaign, error) {
+	var campaigns []Campaign
+	err := r.db.Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	return campaigns, err
+}
+
+func (r *repository) FindByUserID(userID int) ([]Campaign, error) {
+	var campaigns []Campaign
+	err := r.db.Where("user_id = ?", userID).Preload("CampaignImages", "campaign_images.is_primary = 1").Find(&campaigns).Error
+	return campaigns, err
+}
