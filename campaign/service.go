4d0a455ca11990eee3d0e1785866be751d6c0183
Sugeng Winanjuar <winanjuar@gmail.com>
2021-06-05 13:38:58 +0700

    create campaign end point

--- campaign/service.go
+++ campaign/service.go
@@ -1,8 +1,15 @@
 package campaign
 
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
 type Service interface {
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 }
 
 type service struct {
@@ -26,4 +33,20 @@ func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
 func (s *service) GetCampaignByID(input GetCampaignDetailInput) (Campaign, error) {
 	campaign, err := s.repository.FindByID(input.ID)
 	return campaign, err
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.Perks = input.Perks
+	campaign.GoalAmount = input.GoalAmount
+	campaign.UserID = input.User.ID
+
+	slugCandidate := fmt.Sprintf("%s %d", input.Name, input.User.ID)
+	campaign.Slug = slug.Make(slugCandidate)
+
+	newCampaign, err := s.repository.Save(campaign)
+	return newCampaign, err
 }
