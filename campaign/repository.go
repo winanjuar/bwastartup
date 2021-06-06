031bb944b615d9f240c753a85fae8dc0084e54cb
Sugeng Winanjuar <winanjuar@gmail.com>
2021-06-06 23:17:43 +0700

    update campaign end point

--- campaign/repository.go
+++ campaign/repository.go
@@ -7,6 +7,7 @@ type Repository interface {
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
 	Save(campaign Campaign) (Campaign, error)
+	Update(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -37,5 +38,10 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 
 func (r *repository) Save(campaign Campaign) (Campaign, error) {
 	err := r.db.Create(&campaign).Error
+	return campaign, err
+}
+
+func (r *repository) Update(campaign Campaign) (Campaign, error) {
+	err := r.db.Save(&campaign).Error
 	return campaign, err
 }
