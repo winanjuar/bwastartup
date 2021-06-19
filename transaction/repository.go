b7481612dcb38998b98595a6ef331c83a3c8b2c8
Sugeng Winanjuar <winanjuar@gmail.com>
2021-06-19 12:44:23 +0700

    campaign transaction end point

---
+++ transaction/repository.go
@@ -0,0 +1,21 @@
+package transaction
+
+import "gorm.io/gorm"
+
+type repository struct {
+	db *gorm.DB
+}
+
+type Repository interface {
+	GetByCampaignID(campaignID int) ([]Transaction, error)
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) GetByCampaignID(campaignID int) ([]Transaction, error) {
+	var transactions []Transaction
+	err := r.db.Preload("User").Where("campaign_id = ?", campaignID).Order("id desc").Find(&transactions).Error
+	return transactions, err
+}
---
+++ transaction/service.go
@@ -0,0 +1,33 @@
+package transaction
+
+import (
+	"bwastartup/campaign"
+	"errors"
+)
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	campaign, err := s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("not an owner of the campaign")
+	}
+
+	transaction, err := s.repository.GetByCampaignID(input.ID)
+	return transaction, err
+}
